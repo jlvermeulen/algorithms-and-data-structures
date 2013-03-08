089faa497bddec4cd3b9f7aaae45675dfa6937cf
Jordi Vermeulen <j.l.vermeulen@gmail.com>
2013-03-08 22:16:02 +0100

    Heap and Union Find

---
+++ Code/Utility/Datastructures/Heap.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Utility
+{
+    using System;
+using System.Collections.Generic;
+
+public class MaxHeap<T> : Heap<T>
+    where T : IComparable<T>
+{
+    public MaxHeap() { }
+
+    public MaxHeap(ICollection<T> data) : base(data) { }
+
+    protected override void Heapify(int root)
+    {
+        int p = this.Parent(root);
+
+        if (p != -1 && this.heap[p].CompareTo(this.heap[root]) < 0)
+        {
+            this.Switch(p, root);
+            this.Heapify(p);
+        }
+        else
+        {
+            int l = this.Left(root);
+            int r = this.Right(root);
+
+            if (l != -1 && r != -1)
+            {
+                int max = this.heap[l].CompareTo(this.heap[r]) > 0 ? l : r;
+                if(this.heap[max].CompareTo(this.heap[root]) > 0)
+                {
+                    this.Switch(max, root);
+                    this.Heapify(max);
+                }
+            }
+            else if (l != -1 && this.heap[l].CompareTo(this.heap[root]) > 0)
+            {
+                this.Switch(l, root);
+                this.Heapify(l);
+            }
+            else if (r != -1 && this.heap[r].CompareTo(this.heap[root]) > 0)
+            {
+                this.Switch(r, root);
+                this.Heapify(r);
+            }
+        }
+    }
+}
+
+public class MinHeap<T> : Heap<T>
+    where T : IComparable<T>
+{
+    public MinHeap() { }
+
+    public MinHeap(ICollection<T> data) : base(data) { }
+
+    protected override void Heapify(int root)
+    {
+        int p = this.Parent(root);
+
+        if (p != -1 && this.heap[p].CompareTo(this.heap[root]) > 0)
+        {
+            this.Switch(p, root);
+            this.Heapify(p);
+        }
+        else
+        {
+            int l = this.Left(root);
+            int r = this.Right(root);
+
+            if (l != -1 && r != -1)
+            {
+                int max = this.heap[l].CompareTo(this.heap[r]) < 0 ? l : r;
+                if(this.heap[max].CompareTo(this.heap[root]) < 0)
+                {
+                    this.Switch(max, root);
+                    this.Heapify(max);
+                }
+            }
+            else if (l != -1 && this.heap[l].CompareTo(this.heap[root]) < 0)
+            {
+                this.Switch(l, root);
+                this.Heapify(l);
+            }
+            else if (r != -1 && this.heap[r].CompareTo(this.heap[root]) < 0)
+            {
+                this.Switch(r, root);
+                this.Heapify(r);
+            }
+        }
+    }
+}
+
+public abstract class Heap<T>
+    where T : IComparable<T>
+{
+    protected List<T> heap = new List<T>();
+
+    public Heap() { }
+
+    public Heap(ICollection<T> data)
+    {
+        foreach (T t in data)
+            Add(t);
+    }
+
+    public void Add(T item)
+    {
+        this.heap.Add(item);
+        this.Heapify(this.heap.Count - 1);
+    }
+
+    public T Top()
+    {
+        T top = this.heap[0];
+        this.heap[0] = this.heap[this.heap.Count - 1];
+        this.heap.RemoveAt(this.heap.Count - 1);
+        this.Heapify(0);
+        return top;
+    }
+
+    public T Peek()
+    {
+        return this.heap[0];
+    }
+
+    public int Count { get { return this.heap.Count; } }
+
+    protected abstract void Heapify(int root);
+
+    protected int Parent(int node)
+    {
+        int p = (node + 1) / 2 - 1;
+        if (p >= 0 && p < this.heap.Count)
+            return p;
+        return -1;
+    }
+
+    protected int Left(int node)
+    {
+        int l = (node + 1) * 2 - 1;
+        if (l < this.heap.Count)
+            return l;
+        return -1;
+    }
+
+    protected int Right(int node)
+    {
+        int r = (node + 1) * 2;
+        if (r < this.heap.Count)
+            return r;
+        return -1;
+    }
+
+    protected void Switch(int i1, int i2)
+    {
+        T temp = this.heap[i1];
+        this.heap[i1] = this.heap[i2];
+        this.heap[i2] = temp;
+    }
+}
+}
---
+++ Code/Utility/Datastructures/UnionFind.cs
@@ -0,0 +1,45 @@
+﻿namespace Utility
+{
+    public static class UnionFind<T>
+    {
+        public static UnionNode<T> Make(T value)
+        {
+            UnionNode<T> node = new UnionNode<T>();
+            node.Value = value;
+            node.Parent = node;
+            node.Rank = 0;
+            return node;
+        }
+
+        public static UnionNode<T> Find(UnionNode<T> node)
+        {
+            if (node.Parent != node)
+                node.Parent = Find(node.Parent);
+            return node.Parent;
+        }
+
+        public static void Union(UnionNode<T> node1, UnionNode<T> node2)
+        {
+            UnionNode<T> root1 = Find(node1), root2 = Find(node2);
+            if (root1 == root2)
+                return;
+
+            if (root1.Rank > root2.Rank)
+                root2.Parent = root1;
+            else if (root2.Rank > root1.Rank)
+                root1.Parent = root2;
+            else
+            {
+                root2.Parent = root1;
+                root1.Rank++;
+            }
+        }
+    }
+
+    class UnionNode<T>
+    {
+        public T Value { get; set; }
+        public UnionNode<T> Parent { get; set; }
+        public int Rank { get; set; }
+    }
+}
